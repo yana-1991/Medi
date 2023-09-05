420a838678cada5abe2dbb01b2ecf10cf20a8722
Yana <yananiga91@gmail.com>
2023-09-05 09:43:44 +0300

    Added accordion

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function openCity(evt, cityName) {
-    var i, tabcontent, tablinks;
+function openLink(evt, tabName) {
+    let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -8,9 +8,8 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();

7bbb396b2d1b98a6ddc9a2148363258fa5d937ff
Yana <yananiga91@gmail.com>
2023-09-07 13:36:18 +0300

    Added popup/JS

--- script.js
+++ script.js
@@ -91,7 +91,39 @@ function showCard() {
 }
 
 
-//
+//popup
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose_modal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnClose_modal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+//Handling an Esc_Keypress Event
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+
+//Отправка заявки в ТГ
 
 
 
